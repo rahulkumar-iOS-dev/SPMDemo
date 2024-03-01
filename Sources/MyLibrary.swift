5a27ede8f318d5e230911251dfa188e6177357bd
rahulkumar-iOS-dev <rahulk@dewsolutions.in>
2024-03-01 18:15:16 +0530

    Added Public function

---
+++ Sources/MyLibrary.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by Rahul Kumar on 01/03/24.
+//
+
+import Foundation
+
+public struct MyLibrary {
+    public init() {}
+    
+    public func helloWorld() {
+        print("Hello, World!")
+    }
+}
