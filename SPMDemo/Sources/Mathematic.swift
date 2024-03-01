74536204cda57a516ae09668855aa9bbbbaa9373
rahulkumar-iOS-dev <rahulk@dewsolutions.in>
2024-03-01 17:24:04 +0530

    Added SPM and area function

---
+++ SPMDemo/Sources/Mathematic.swift
@@ -0,0 +1,29 @@
+//
+//  File.swift
+//  
+//
+//  Created by Rahul Kumar on 01/03/24.
+//
+
+import Foundation
+public enum Shape {
+    case Circle
+    case Triangle
+    case Square
+}
+
+public class Mathematic {
+    public func area(of shape: Shape, with radius: CGFloat?, base: CGFloat?, height: CGFloat?, length: CGFloat?) -> CGFloat {
+        switch shape {
+        case .Circle:
+            guard let radius else { return nil }
+            return radius * radius
+        case .Triangle:
+            guard let base, let height else { return nil }
+            return 0.5 * base * height
+        case .Square:
+            guard let length else { return nil }
+            return length * length
+        }
+    }
+}
